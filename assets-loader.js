ba9f2b7d5005d7396dbf2ecf3503885607083007
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2025-02-14 11:23:37 +0200

    Created an asset loader for svgs

---
+++ assets-loader.js
@@ -0,0 +1,18 @@
+async function loadAssets() {
+  let assetContainers = document.querySelectorAll(".asset");
+  console.log(assetContainers);
+  for (const container of assetContainers) {
+    const asset = container.getAttribute('data-asset');
+    const asset_class = container.getAttribute('data-asset-class');
+    let data = await fetch(`/assets/${asset}`);
+    let text = await data.text();
+
+    const frg = document.createElement('div');
+    frg.innerHTML = text;
+    frg.querySelector('svg').setAttribute('class', asset_class);
+
+    container.append(frg.querySelector('svg'));
+  }
+}
+
+loadAssets();

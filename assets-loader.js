fec0a68d574f84dbd30b1d30907379a10cedfea7
devark28 <e.shimwa1@alustudent.com>
2025-02-14 11:17:17 +0200

    Auto published at Fri Feb 14 11:17:17 AM CAT 2025

--- assets-loader.js
+++ assets-loader.js
@@ -6,7 +6,7 @@ async function loadAssets() {
     let data = await fetch(`/assets/${asset}`);
     let text = await data.text();
     console.log(text);
-    const frg = document.createElement('svg');
+    const frg = document.createElement('div');
     frg.innerHTML = text;
     container.append(frg);
   }

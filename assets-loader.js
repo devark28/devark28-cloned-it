09a541d76f1eb6a7e43b9a5555647b010a14eaf6
devark28 <e.shimwa1@alustudent.com>
2025-02-14 10:58:30 +0200

    Auto published at Fri Feb 14 10:58:30 AM CAT 2025

--- assets-loader.js
+++ assets-loader.js
@@ -4,7 +4,7 @@ async function loadAssets() {
   for (const container of assetContainers) {
     const asset = container.getAttribute('data-asset');
     let data = await fetch(`/assets/${asset}`);
-    assetContainers.prepend()
+    assetContainers.innerHtml = data;
   }
 }
 

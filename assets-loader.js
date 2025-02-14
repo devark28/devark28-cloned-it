8dd7bb4da18c7d8e10a4dcd351e2ee8702b5783c
devark28 <e.shimwa1@alustudent.com>
2025-02-14 10:57:22 +0200

    Auto published at Fri Feb 14 10:57:22 AM CAT 2025

--- assets-loader.js
+++ assets-loader.js
@@ -1,10 +1,10 @@
 function loadAssets() {
   let assetContainers = document.querySelectorAll(".asset");
   console.log(assetContainers);
-  assetContainers.forEach((container) => {
+  for (const container of assetContainers) {
     const asset = container.getAttribute('data-asset');
     let data = await fetch(`/assets/${asset}`);
-  })
+  }
 }
 
 loadAssets();

52512d933402ede17a4945cba406613356652bfe
devark28 <e.shimwa1@alustudent.com>
2025-02-14 11:03:58 +0200

    Auto published at Fri Feb 14 11:03:58 AM CAT 2025

--- assets-loader.js
+++ assets-loader.js
@@ -1,5 +1,5 @@
 async function loadAssets() {
-  let assetContainers = document.querySelectorAll(".asset");
+  let assetContainers = document.getElementsByClassName(".asset");
   console.log(assetContainers);
   for (const container of assetContainers) {
     const asset = container.getAttribute('data-asset');
